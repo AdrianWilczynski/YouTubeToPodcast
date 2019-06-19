695ac0dc0ea91159c997be1fca532071761a6e3f
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-06-19 19:10:10 +0200

    Try to reduce server load to stop exceeding quotas on free plan.

--- src/YouTubeToPodcast/Constants.cs
+++ src/YouTubeToPodcast/Constants.cs
@@ -4,13 +4,13 @@ namespace YouTubeToPodcast
     {
         public static class Cache
         {
-            public const int MinutesTillExpiration = 30;
+            public const int MinutesTillExpiration = 60;
         }
 
         public static class YouTube
         {
             public const int MaxResults = 50;
-            public const int MaxRequests = 10;
+            public const int MaxRequests = 1;
             public const string AudioFileExtension = ".m4a";
             public const string AudioMimeType = "audio/m4a";
         }
